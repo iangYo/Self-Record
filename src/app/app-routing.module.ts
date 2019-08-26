83ae4e0bd3695b6c4ab79a5d67babc3e417c14dc
SelfRecord <selfrecordapp@gmail.com>
2019-08-26 11:16:33 -0300

    Estilização e cards adicionados na tela remedio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'pagina-inicial', pathMatch: 'full' },
+  { path: '', redirectTo: 'remedio', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   { path: 'cadastro-usuario', loadChildren: './pages/cadastro-usuario/cadastro-usuario.module#CadastroUsuarioPageModule' },
   { path: 'cadastro-profissional', loadChildren: './pages/cadastro-profissional/cadastro-profissional.module#CadastroProfissionalPageModule' },
   { path: 'cadastro-geral', loadChildren: './pages/cadastro-geral/cadastro-geral.module#CadastroGeralPageModule' },
   { path: 'pagina-inicial', loadChildren: './pages/pagina-inicial/pagina-inicial.module#PaginaInicialPageModule' },
-  { path: 'test-cadastro-usuario', loadChildren: './pages/test-cadastro-usuario/test-cadastro-usuario.module#TestCadastroUsuarioPageModule' },
   { path: 'perfil-usuario', loadChildren: './pages/perfil-usuario/perfil-usuario.module#PerfilUsuarioPageModule' },
   { path: 'remedio', loadChildren: './pages/remedio/remedio.module#RemedioPageModule' },
 ];
